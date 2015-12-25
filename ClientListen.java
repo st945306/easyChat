6cf90a7644d86ff51a481cbef55302e73a2fa720
CakeCyanide <cakecyanide@gmail.com>
2015-12-26 03:04:02 +0800

    uh... fix bugs again

--- ClientListen.java
+++ ClientListen.java
@@ -7,11 +7,11 @@ public class ClientListen extends Thread {
 	String targetUser;
 	String msg;
 
-	public ClientListen(Client cl, ClientGUI clg, JTextArea ms) {
+	public ClientListen(Client cl, ClientGUI clg, JTextArea ms, String u) {
 		client = cl;
 		clientGUI = clg;
 		msgToDisplay = ms;
-		//targetUser = u;
+		targetUser = u;
 	}
 
 	@Override
